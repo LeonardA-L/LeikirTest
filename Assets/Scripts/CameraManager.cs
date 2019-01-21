06ced749a5443f8b8a519fcfe2531d78c4fb42ba
LeonardA-L <leonard@allain-launay.fr>
2019-01-21 20:38:23 +0100

    Init scene and add basic controller

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LeikirTest
+{
+    public class CameraManager : Singleton<CameraManager>
+    {
+        protected CameraManager() { }
+
+        public float m_distance = 12;
+
+        private Transform m_cam;
+        private Transform m_character;
+
+        void Start()
+        {
+            m_cam = Camera.main.transform;
+            var character = (LeikirTest.CharacterController)FindObjectOfType(typeof(LeikirTest.CharacterController)); // Assume there is only one character
+            // In an actual production, player character(s) would be dealt by a PlayerManager, which would have a GetPlayers() accessor
+            m_character = character.transform;
+        }
+
+        void Update()
+        {
+            m_cam.transform.position = Vector3.Lerp(m_cam.transform.position, m_character.position - m_cam.forward * m_distance, 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace LeikirTest
+{
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterController : MonoBehaviour
+    {
+        private Rigidbody rb;
+        private Vector3 lastDirection;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            lastDirection = transform.forward;
+        }
+
+        void FixedUpdate()
+        {
+            lastDirection = ReadInputForceGamepad();
+            rb.MovePosition(rb.position + lastDirection);
+        }
+
+        void Update()
+        {
+            transform.forward = Vector3.Slerp(transform.forward, lastDirection, 10 * Time.deltaTime);   // TODO move to member
+                                                                                                        //Note: Here the direction lerp is purely cosmetic. Direction does not impact movement (cf how FixedUpdate does not take direction into account)
+        }
+
+        Vector3 ReadInputForce()
+        {
+            return new Vector3(1, 0, 0);
+        }
+
+        Vector3 ReadInputForceGamepad()
+        {
+            Transform cam = Camera.main.transform;          // TODO move to member
+            Debug.Assert(cam != null, "No Main Camera");
+
+            Vector2 joy = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            joy.Normalize();
+            joy *= 0.1f;    // TODO maxSpeed member
+
+            Vector3 camForward = cam.forward;
+            camForward.y = 0;
+            //Debug.Assert(camForward.sqrMagnitude != 0);   // With the hack'n'slash view we assume we don't need this
+            camForward.Normalize();
+
+            return joy.y * camForward
+                 + joy.x * cam.right;
+        }
+    }
+}
