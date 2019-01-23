34fff05d768f115d5cc43c5099de0db8ccf8a71e
LeonardA-L <leonard@allain-launay.fr>
2019-01-23 17:55:12 +0100

    Wrap up and documentation

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 namespace LeikirTest
 {
+    /*
+     * Control strategy reading the movement command from an "AI".
+     * (for the sake of time it's not AI per se as it isn't intelligent at all)
+     * Will follow a path consisting in a list of target points, one point after the other.
+     * Each instance of the strategy can define its own path.
+     * (see ControlStrategy.cs)
+     */
     [CreateAssetMenu(menuName = "Control Strategies/AI Control")]
     public class AIControl : ControlStrategy
     {
+        [Tooltip("List of points defining the path of the character. Each point is relative to the starting point of the character")]
         public List<Vector3> m_path;
+        [Tooltip("Starting index of the target point in the path")]
         public int m_currentTargetIdx = 0;
+        [Tooltip("Threshold under which the character is close enough to its target to move on to the next point in the path")]
         public float m_distanceThreshold = 0.1f;
 
         private Transform m_controllerTransform;
@@ -22,15 +32,21 @@ namespace LeikirTest
 
         public override Vector3 ReadInputMovement()
         {
+            Debug.Assert(m_initialPosition != null, "Initial Position has not been initialized.");  // Right now we can't hotswap strategies
+            // Compute target position
             Vector3 targetPosition = m_initialPosition + m_path[m_currentTargetIdx];
             Vector3 targetDirection = Vector3.zero;
 
+            // Check if we are close enough to the target to allow moving on to the next
             if ((targetPosition - m_controllerTransform.position).magnitude < m_distanceThreshold)
             {
                 m_currentTargetIdx = (m_currentTargetIdx + 1) % m_path.Count;
-            } else
+            }
+            // Else define direction to the target
+            else
             {
                 targetDirection = MaxSpeed * (targetPosition - m_controllerTransform.position).normalized;
+                // Note: This could be lerped for fancier, less robotic turns.
             }
 
             return targetDirection;
--- Assets/Scripts/ControlStrategy.cs
+++ Assets/Scripts/ControlStrategy.cs
@@ -2,9 +2,19 @@
 
 namespace LeikirTest
 {
+    /*
+     * A Control Strategy is used by the CharacterController to fetch inputs to move a character.
+     * A child class must implement ReadInputMovement() to define a way to decide on the next movement
+     * For the character.
+     * For instance, a Gamepad strategy will read input from a user game pad.
+     * An AI strategy will compute the next movement from a scripted AI.
+     * An Online strategy will read input over the network from a remote player.
+     */
     public abstract class ControlStrategy : ScriptableObject
     {
+        [Tooltip("Rotation speed of the character (does not impact movement)")]
         public float m_rotationSpeed = 10;
+        [Tooltip("Max speed for the character")]
         public float m_maxSpeed = 7;
 
         public abstract Vector3 ReadInputMovement();
